71d90e18358be426f23407f2eb7dd2e98b8d7d4f
ambroslins <ambros.lins@gmail.com>
2022-05-05 10:27:53 +0200

    Switch to dynamic dispatch
    The handler for `WithLog` looks strange,
    however this is the only way I manged to get the types right.

--- effectful-co-log-core/src/Effectful/Colog/Core/Effect.hs
+++ effectful-co-log-core/src/Effectful/Colog/Core/Effect.hs
@@ -8,33 +8,26 @@ module Effectful.Colog.Core.Effect
   )
 where
 
-import Colog.Core (LogAction (..))
-import qualified Colog.Core as Colog
+import Colog.Core (LogAction (..), hoistLogAction)
 import Data.Foldable (traverse_)
-import Effectful (Dispatch (Static), DispatchOf, Effect, type (:>))
-import Effectful.Dispatch.Static
-  ( SideEffects (NoSideEffects),
-    StaticRep,
-    evalStaticRep,
-    localStaticRep,
-    unEff,
-    unsafeEff,
-    unsafeEff_,
+import Effectful
+  ( Dispatch (Dynamic),
+    DispatchOf,
+    Eff,
+    Effect,
+    UnliftStrategy (SeqUnlift),
+    raise,
+    type (:>),
   )
-import Effectful.Dispatch.Static.Primitive
-  ( forkEnv,
-    getEnv,
-  )
-import Effectful.Internal.Monad
-  ( Eff,
-  )
+import Effectful.Dispatch.Dynamic (localLiftUnlift, reinterpret, send)
+import Effectful.Reader.Static (ask, local, runReader)
 
 -- | An effect for composable, contravariant and comonadic logging using the @co-log@ library.
-data Log msg :: Effect
+data Log msg :: Effect where
+  LogMsg :: msg -> Log msg m ()
+  WithLog :: (LogAction m msg -> LogAction m msg) -> m a -> Log msg m a
 
-type instance DispatchOf (Log msg) = 'Static 'NoSideEffects
-
-newtype instance StaticRep (Log msg) = Log (LogAction IO msg)
+type instance DispatchOf (Log msg) = 'Dynamic
 
 -- | 'LogAction' that works directly with the 'Eff' monad.
 type LogActionEff es msg = LogAction (Eff es) msg
@@ -43,17 +36,23 @@ type LogActionEff es msg = LogAction (Eff es) msg
 --
 -- This function is the effectful version of 'Colog.usingLoggerT'
 runLog :: forall msg a es. LogActionEff es msg -> Eff (Log msg ': es) a -> Eff es a
-runLog action m = do
-  env <- unsafeEff forkEnv
-  evalStaticRep (Log $ Colog.hoistLogAction (`unEff` env) action) m
+runLog logAction = reinterpret (runReader logAction) $ \env -> \case
+  LogMsg msg -> do
+    LogAction action <- ask
+    raise $ action msg
+  -- This implementation looks and feels strange,
+  -- however this is the only way I would get it to typecheck.
+  WithLog f m -> localLiftUnlift env SeqUnlift $ \lift unlift -> do
+    action <- ask
+    let f' :: LogActionEff es msg -> LogActionEff es msg
+        f' = hoistLogAction (runReader action . unlift) . f . hoistLogAction (lift . raise)
+    local f' (unlift m)
 
 -- | Perform logging action with given @msg@.
 --
 -- The effectful version of 'Colog.logMsg'.
 logMsg :: forall msg es. Log msg :> es => msg -> Eff es ()
-logMsg msg = unsafeEff $ \env -> do
-  Log (LogAction action) <- getEnv env
-  action msg
+logMsg = send . LogMsg
 
 -- | The effectful version of 'Colog.logMsgs'.
 logMsgs :: forall msg f es. (Foldable f, Log msg :> es) => f msg -> Eff es ()
@@ -61,7 +60,4 @@ logMsgs = traverse_ logMsg
 
 -- | The effectful version of 'Colog.withLog'.
 withLog :: forall msg a es. Log msg :> es => (LogActionEff es msg -> LogActionEff es msg) -> Eff es a -> Eff es a
-withLog f m = do
-  env <- unsafeEff forkEnv
-  let f' = Colog.hoistLogAction (`unEff` env) . f . Colog.hoistLogAction unsafeEff_
-  localStaticRep (\(Log action) -> Log (f' action)) m
+withLog f m = send (WithLog f m)
